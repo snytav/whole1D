832b2f7415a2b837f6fafdcd85d96bbb337bb55b
Alexey Snytnikov <snytav@gmail.com>
2024-09-18 23:09:33 +0200

    jacobian for whole domain OK

--- loss_func.py
+++ loss_func.py
@@ -1,23 +1,39 @@
-import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd.functional import jacobian,hessian
 from deriv_example import neural_network_x,neural_network,nx
-from deriv_example import W,x_space,pn
+from deriv_example import W,x_space,pn,sigmoid
+import autograd.numpy as np
+from WholePoisNet import WholePoisNet,w,w1,xt
+from autograd import grad
 
 
-x0 =x_space[0]
+
+if __name__ == "__main__":
+    yn = np.array([neural_network(W, xi)[0][0] for xi in x_space])
+    wh = WholePoisNet(nx)
+    wh.fc1.set_weight(w)   #weight = nn.Parameter(w3.T)
+    wh.fc2.weight = nn.Parameter(w1)
+    yt = wh(xt)
+    d_yn_dx = []
+    for xi in x_space:
+        #net_out = neural_network(W, xi)[0][0]
+
+        net_out_d = grad(neural_network_x)(xi)
+        d_yn_dx.append(net_out_d)
+
+    d_yn_dx = np.array(d_yn_dx)
+    from torch.autograd.functional import jacobian
+
+    jac = jacobian(wh.forward,inputs=xt)
+    d_yt_dx = jac[:, 0, :].diag()
+
+    dy = np.max(np.abs(d_yn_dx - d_yt_dx.detach().numpy()))
+
+    qq = 0
 
 
-y = neural_network(W,x0)
-yt = pn.forward(x0)
-dy = np.abs(y-yt.item())
 
-net_out_arr = []
-for xi in x_space:
-    net_out = neural_network(W, xi)[0][0]
-    net_out_arr.append(net_out)
-net_out_arr = np.array(net_out_arr)
 
 
 qq = 0
